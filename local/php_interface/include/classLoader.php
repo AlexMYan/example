b3c85f1d5f58b90d23442a6efea5eb02dcd7ea6d
Yanovich Alexandr <yanovicham@gmail.com>
2022-06-29 15:33:14 +0300

    new

--- local/php_interface/include/classLoader.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-\Bitrix\Main\Loader::registerAutoLoadClasses(null, [
-    'Extra\Helper' => '/local/php_interface/classes/Extra/Helper.php',
-    'Custom\QuantitativeAccounting' => '/local/php_interface/classes/Custom/QuantitativeAccounting.php',
-    'MyPsr\FilesLogger' => '/local/php_interface/classes/MyPsr/FilesLogger.php',
-    'MyPsr\Test\FilesLoggerTest' => '/local/php_interface/classes/MyPsr/Test/FilesLoggerTest.php',
-    'MyPsr\Test\SimpleTest ' => '/local/php_interface/classes/MyPsr/Test/SimpleTest .php',
-]);
